2a8eccb664c08d011af8e1d32385dde1e00edd96
Iulia-Soimaru & Michael Pérez <pair+iulia-soimaru.michaeldperez@devbootcamp.com>
2015-03-23 16:56:52 -0700

    Add complete functionality

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,65 @@
-var newTodoList = function() {
-	// ???
+var Task = function(todoList, description){
+  this.todoList = todoList;
+  this.id = Task.counter++;
+  this.description = description;
+  this.completed = false;
+};
+
+Task.counter = 0;
+
+Task.prototype.complete = function(){
+  this.completed = true;
+};
+
+Task.prototype.remove = function(){
+  this.todoList.remove(this);
 };
 
 
 
-// Driver code
+var TodoList = function(){
+  this.tasks = [];
+};
 
 
-var todoList = newTodoList();
+// groceryList.add('bread');
+TodoList.prototype.add = function(description){
+  var task = new Task(this, description)
+  this.tasks.push(task);
+};
+
+// groceryList.remove(Task);
+TodoList.prototype.remove = function(task){
+  var index = this.tasks.indexOf(task);
+  this.tasks.splice(index, 1);
+};
+
+TodoList.prototype.list = function(){
+  for (var i = 0; i < this.tasks.length; i++){
+    console.log(this.tasks[i].id + ": " + this.tasks[i].description + " completed: " + this.tasks[i].completed);
+  };
+};
+
+
+var groceryList = new TodoList();
+// var bread = Object.create(task)
+// bread.description = "bread"
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+// groceryList.list();
+
+var breadTask = groceryList.tasks[0];
+console.log(breadTask.description);
+console.log(breadTask.completed);
+breadTask.complete();
+// groceryList.list();
+//
+breadTask.remove();
+// groceryList.remove(breadTask);
+groceryList.list();
+
+
+
+
+
